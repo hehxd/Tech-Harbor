72c2e501a584c2b9f8de2cddc286045e17550075
Nikola <nikolajkan8@gmail.com>
2024-02-06 20:58:28 +0100

    Initial commit

---
+++ src/main/java/tech/techharbor/TechHarborApplication.java
@@ -0,0 +1,13 @@
+package tech.techharbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHarborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHarborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/techharbor/TechHarborApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.techharbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHarborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
