0e8202c1a9d701fd23d3b9be746c8c2cf8485d3c
Nikola <nikolajkan8@gmail.com>
2024-02-07 22:18:09 +0100

    Revert "Updated the app design, made controllers for products, homepage and categories. App product display should be working fine, also individual product page and category page."
    This reverts commit c19dfcdf7525f56c3a33ed0b896129a41505d3b2.

--- src/main/java/tech/techharbor/Model/Exceptions/CategoryNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package tech.techharbor.Model.Exceptions;
-
-public class CategoryNotFoundException extends RuntimeException{
-    public CategoryNotFoundException(Integer categoryId) {
-        super(String.format("Category with id %d does not exist.", categoryId));
-    }
-
-}
--- src/main/java/tech/techharbor/Model/Exceptions/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package tech.techharbor.Model.Exceptions;
-
-public class ProductNotFoundException extends RuntimeException{
-
-    public ProductNotFoundException(Integer productId) {
-        super(String.format("Product with id %d does not exist.", productId));
-    }
-}
