7c8a08d633b17df1689a5e67d362c173bf3e5cda
Nikola <nikolajkan8@gmail.com>
2024-02-07 22:02:49 +0100

    cleaned imports.

--- src/main/java/tech/techharbor/Repository/CategoryRepository.java
+++ src/main/java/tech/techharbor/Repository/CategoryRepository.java
@@ -1,9 +1,7 @@
 package tech.techharbor.Repository;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.techharbor.Model.CategoryModel;
-import java.util.List;
 
 public interface CategoryRepository extends JpaRepository<CategoryModel, Integer> {
 
